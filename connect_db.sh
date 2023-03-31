d06ad7cc7c2f25e00e2df5bb6b468e9bd98ba532
Nabela Ashraf <nabelaashraf1999@gmail.com>
2023-04-01 00:49:48 +0200

    Add files via upload

---
+++ connect_db.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function connectDB {
+echo "Please enter the name of the database you want to connect :" 
+read DBname
+
+if [ -d $DBname ]; then
+	cd ~/$DBname
+	echo "connected to $DBname successfully"
+else
+	echo "Database doesn't exist , enter a valid database "
+fi
+}
