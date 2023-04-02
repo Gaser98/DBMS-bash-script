70afe48d0fa76e0ada69cbde7ffba13f6bb4f640
Gaser98 <76227165+Gaser98@users.noreply.github.com>
2023-04-02 06:08:41 +0200

    Add files via upload

---
+++ createTable.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+createTable() {
+    table_name=$1
+    shift
+    columns="$@"
+
+    # Check for null values in column names 
+    for col in "${columns[@]}"; do
+        if [ -z "$col" ]; then
+            echo "Column names cannot be empty"
+            exit 1
+        fi
+    done
+    # Check if table already exists
+    if [[ -f "${table_name}.txt" ]]; then
+        echo "Table ${table_name} already exists"
+        return
+    fi
+    createTable
+    # Create table file
+    touch "${table_name}.txt"
+
+    # Add column names to file
+    echo "$columns" > "${table_name}.txt"
+
+    echo "Table ${table_name} created successfully"
+}
+
