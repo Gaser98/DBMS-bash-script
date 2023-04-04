ef5433718973bdf2efc21cb6291ddc6c8b721349
Nabela Ashraf <nabelaashraf1999@gmail.com>
2023-04-04 03:36:28 +0200

    Add files via upload

---
+++ select_table.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#function to select a table from database 
+function select {
+	echo -e "Enter the name of the table you want to select from : \c"
+	read tname
+	if [[ -f database/$tname ]] then
+		select item in "Select the whole table" "Select a field from the table" "Exit"
+		do
+			case $REPLY in
+				1)
+					column -t -s ';' $tname 2>>/dev/null
+					tablemenu
+					;;
+				2) echo -e "Enter number of the column you want to select : \c"
+					read num
+					awk -F: '{print $num}' database/tname
+					tablemenu
+					;;
+				3) exit
+					;;
+			esac
+		done
+	else
+		echo "Enter a valid table name"
+				tablemenu
+}	
