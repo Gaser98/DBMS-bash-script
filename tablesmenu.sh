47b25d348cb706877c3ab8d3bc18ba6ef2b44272
Gaser98 <76227165+Gaser98@users.noreply.github.com>
2023-04-06 06:09:03 +0200

    Update tablesmenu.sh

--- tablesmenu.sh
+++ tablesmenu.sh
@@ -3,27 +3,26 @@ function tablesmenu()
 {
         echo "Please select 1 to create a new table"
         echo "Please select 2 to list available tables"
-        echo "Please select 3 to drop a table"
-        echo "Please select 4 to insert into a table"
-        echo "Please select 5 to select from a table"
-        echo "Please select 6 to delete from a table"
+        echo "Please select 3 to insert into a table"
+        echo "Please select 4 to select from a table"
+        echo "Please select 5 to delete from a table"
+        echo "Please select 6 to update a table"
+        echo "Please select 7 to exit"
         read op
         case $op in
                 1)createTable
                         ;;
                 2)ls .; tablesmenu
                         ;;
-                3)dropTable
+                3)insertIntoTable
                         ;;
-                4)insertIntoTable
+                4)selectTable
                         ;;
-                5)selectTable
+                5)deleteTable #delete or drop table
                         ;;
-                6)deleteTable
-                        ;;
-                7)updateTable
+                6)updateTable
                         ;;        
-		8) exit
+		7) exit
 			;;
                 *)echo "Invalid option..choose again"
                         ;;
