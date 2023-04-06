24eb62b8dd7de98812dcf5e76fe2f31df92a6314
Gaser98 <76227165+Gaser98@users.noreply.github.com>
2023-04-06 13:27:06 +0200

    Update updateTable.sh

--- updateTable.sh
+++ updateTable.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
-function updateTable (){
-
-echo "Enter the table_name:"
-read tname
-
-echo "Enter the row number:"
-read row
-
-echo "Enter the column number(s) you wish to modify:"
-read columns
-
-echo "Enter the new data:"
-read new_data
-
-awk -v row="$row" -v columns="$columns" -v new_data="$new_data" \
-'BEGIN {FS=OFS="\t"} NR==row {split(columns, c, ":"); \
-for (i in c) $c[i] = new_data} {print}' \
-$tname.txt > tmp && mv tmp $tname.txt
-
-echo "Fields updated successfully!"
+function updateTable {
+    echo "Enter the table_name:"
+    read tname
+    echo "Enter the row:"
+    read row
+    echo "Enter the column number(s) you wish to modify:"
+    read columns
+    echo "Enter the new data:"
+    read new_data
+    awk -v row=$row -v columns=$columns -v new_data=$new_data \
+        'BEGIN {FS=OFS=":"} NR==row {split(columns,c,":"); \
+         for (i in c) $c[i]=new_data} {print}' $tname.txt > tmp.txt && mv tmp.txt $tname.txt
+    echo "Fields updated successfully!"
 }
