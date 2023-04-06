d0bde71284d1d423e3d8f0ec3fee119a8f3a6926
Gaser98 <76227165+Gaser98@users.noreply.github.com>
2023-04-06 13:26:04 +0200

    Add files via upload

--- DBMS.sh
+++ DBMS.sh
@@ -235,32 +235,15 @@ function selectTable {
 function updateTable {
     echo "Enter the table_name:"
     read tname
-
     echo "Enter the row:"
-    read r
-
+    read row
     echo "Enter the column number(s) you wish to modify:"
-    read c
-
+    read columns
     echo "Enter the new data:"
     read new_data
-
-    awk -v row="$r" -v columns="$c" -v new_data=$n \
-    'BEGIN{FS=OFS=":"} 
-    {
-        split($0, fields, FS)
-        if (NR == $r) {
-            split($c, cols, ":")
-            for (i in cols) {
-                fields[cols[i]] = n
-            }
-        }
-        for (i = 1; i <= NF; i++) {
-            printf("%s%s", fields[i], (i == NF) ? "\n" : FS)
-        }
-    }' $tname.txt > temp.txt && mv temp.txt $tname.txt
-
+    awk -v row=$row -v columns=$columns -v new_data=$new_data \
+        'BEGIN {FS=OFS=":"} NR==row {split(columns,c,":"); \
+         for (i in c) $c[i]=new_data} {print}' $tname.txt > tmp.txt && mv tmp.txt $tname.txt
     echo "Fields updated successfully!"
 }
-
 menu
